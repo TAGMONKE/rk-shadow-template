28d89e527b5036c35616d1c382d970e830fbe5cf
TAGMONKE <104656089+TAGMONKE@users.noreply.github.com>
2024-07-12 11:21:18 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.rkshadow.gorillatag.menutemplate";
+        public const string Name = "RK Shadow Template (change in PluginInfo.cs)";
+        public const string Description = "Created by @goldentrophy and @tagmonkevr with love <3";
+        public const string Version = "1.1.0";
+    }
+}
